b212da6beac8e26677043117080da7fb3d2cf15b
Doni Setiawan <donisetiawan.050@gmail.com>
2018-12-27 16:50:09 +0700

    Add style

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,19 @@
 import React, { PureComponent } from "react";
 
+import "./Home.css";
+
 class Home extends PureComponent {
   render() {
-    return <h1>I'm Home component</h1>;
+    return (
+      <div className="Home">
+        <h1>Welcome to Codejobs</h1>
+        <p>
+          In this recipe you will learn how to add styles to components. If you
+          want to learn more you can visit our Youtube Channel at
+          <a href="http://youtube.com/codejobs"> Codejobs</a>.
+        </p>
+      </div>
+    );
   }
 }
 
