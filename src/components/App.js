1f7cf1c9369e2cb0ac98adcdb5e062710ec49d7f
Doni Setiawan <donisetiawan.050@gmail.com>
2018-12-27 17:13:28 +0700

    Add header and passing props

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import logo from "../shared/images/logo.svg";
+import React, {Component} from "react";
+import Header from "../shared/components/layout/Header";
 
 import Home from "./Home/Home";
 import "./App.css";
@@ -8,10 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Header title="Welcome to Codejobs" />
         <Home />
       </div>
     );
