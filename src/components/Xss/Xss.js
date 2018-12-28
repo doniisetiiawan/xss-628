88dd207a421a4f8c52e2d8de54f9137ab1aae9e6
Doni Setiawan <donisetiawan.050@gmail.com>
2018-12-28 17:20:07 +0700

    Add xss component

---
+++ src/components/Xss/Xss.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+const response = [
+  {
+    id: 1,
+    title: "My blog post 1...",
+    content: "<p>This is <strong>HTML</strong> code</p>"
+  },
+  {
+    id: 2,
+    title: "My blog post 2...",
+    content: `<p>Alert: <script>alert(1);</script></p>`
+  },
+  {
+    id: 3,
+    title: "My blog post 3...",
+    content: `
+      <p>
+      <img onmouseover="alert('This site is not secure');" src="attack.jpg" />
+      </p>
+    `
+  }
+];
+
+const initialState = JSON.stringify(response);
+
+class Xss extends Component {
+  render() {
+    const posts = JSON.parse(initialState);
+
+    return (
+      <div className="Xss">
+        {posts.map((post, key) => (
+          <div key={key}>
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Xss;
